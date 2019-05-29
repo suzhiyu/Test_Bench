960bd4c2f01f2e0aafa57a9d3a957ca140802f8d
suzhiyu <suzhiyu@suzhiyu-raindeMacBook.local>
2019-05-29 10:05:53 +0800

    mock

--- lianxi.py
+++ lianxi.py
@@ -1,52 +1,43 @@
-#coding=utf-8
-#贷款总额loanamount，借款利率rate 、借款期数periods
-import math
-#借款本金
-#借款利息
-#借款月利率
-#借款期限
+from flask import jsonify
+from flask import Flask
+import requests
+from flask import render_template
 
-#等额本息算法每期还款本息和公式：
-class huankuanjisuan:
+app=Flask(__name__)
+app.config['JSON_AS_ASCII'] = False
 
+@app.route("/yunyingshangmock",methods=["GET"])
 
-    def dengebenxi(loanamount,rate,periods):
-        rates = rate / 12 / 100
-        # 每期还款本息和
-        meiqihuankuane=round(loanamount*rates*math.pow(1+rates,periods)/(math.pow(1+rates,periods)-1),2)
-        #每期还款本金
+def mockhtml():
+
+    return render_template("yunyingshangmock.html")
 
 
-        #每期还款利息
+@app.route("/yunyingshangmockcallback",methods=["post"])
 
-        # 还款总金额
-        huankuanzonge=round(loanamount*rates*math.pow(1+rates,periods)/(math.pow(1+rates,periods)-1)*periods,2)
-        print(meiqihuankuane,huankuanzonge)
-        return meiqihuankuane,huankuanzonge
-    def dengbendengxi(loanamount,rate,periods):
-        #月利率
-        rates=rate/12/100
-        #每期还款金额=本金*（1+年利率)/总期数
-        meiqihuankuane1=round(loanamount*rates+loanamount/periods,2)
-        #print(meiqihuankuane1)
-        #print(round(meiqihuankuane1*periods,2))
 
-        huankuanzonge1=round(loanamount*rates*periods+loanamount,2)
-        #print(huankuanzonge1)
-        print(meiqihuankuane1, huankuanzonge1)
-        return meiqihuankuane1,huankuanzonge1
 
-    def huankuanjisuan(loanamount,rate,periods,gongshi):
+def yunyingshangmockcallback():
 
-        if gongshi == 'dengebenxi':
-                return huankuanjisuan.dengebenxi(loanamount, rate, periods)
-        elif gongshi == 'dengbendengxi':
-                return  huankuanjisuan.dengbendengxi(loanamount, rate, periods)
-        else:
-            return print("++算法输入错误+++")
+
+    #req=requests.post()
+
+
+    return jsonify({"status":"成功"})
+
+def yunyingshangmockreport():
+
+    return
+
 
 
 if __name__ == '__main__':
-    aa=huankuanjisuan
-    aa.huankuanjisuan(5000,36,3,"dengbendengxi")
+    app.run(host='192.168.37.25',
+            port='9898',
+            debug=True)
+
+
+
+
+
 
